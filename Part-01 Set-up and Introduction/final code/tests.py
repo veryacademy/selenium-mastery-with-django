43b3b87f0a7ed42cafe1468a73740d5382b93885
Zander <veryacademy@gmail.com>
2021-04-25 22:04:12 +0100

    Update tests.py

--- Part-01 Set-up and Introduction/final code/tests.py
+++ Part-01 Set-up and Introduction/final code/tests.py
@@ -9,6 +9,7 @@ class Hosttest(LiveServerTestCase):
   	
 	def testhomepage(self):
 		driver = webdriver.Chrome()
+		# try driver = webdriver.Chrome('./chromedriver') with the driver in the project folder if you cant set to path
 
 		driver.get('http://127.0.0.1:8000/')
 		# try driver.get(self.live_server_url) if driver.get('http://127.0.0.1:8000/') does not work
